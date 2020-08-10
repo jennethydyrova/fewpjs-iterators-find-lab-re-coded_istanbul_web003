16a13322d5d43de96552398c7952efb82920328c
jennethydyrova <jennethydyrovaa@gmail.com>
2020-08-11 00:15:47 +0300

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-const testVar = {}
+const testVar = {};
 
 function testFunc() {
-  return "hi"
+  return "hi";
 }
+const record = [
+  { year: "2018", result: "L" },
+  { year: "2017", result: "W" },
+  { year: "2016", result: "N/A" },
+];
+
+const superbowlWin = (arr) => {
+  let win = arr.find((e) => e.result === "W");
+  return win ? win.year : undefined;
+};
+console.log(superbowlWin(record));
